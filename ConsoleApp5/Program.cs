3aaa54aaecfdf4bc659802a47048ed38dd34330f
Mihai Nechifor <Mihai.Nechifor@endava.com>
2019-10-18 18:45:58 +0300

    chnages

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -10,6 +10,27 @@ namespace ConsoleApp5
     {
         static void Main(string[] args)
         {
+            //LinkedListExample();
+        }
+
+        private static void LinkedListExample()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+
+            LinkedListNode<int> node1 = list.AddFirst(10);
+
+            LinkedListNode<int> node2 = new LinkedListNode<int>(20);
+
+            list.AddAfter(node1, node2);
+
+            list.AddLast(30);
+
+            foreach (int node in list)
+            {
+                Console.WriteLine(node);
+            }
+
+            Console.ReadLine();
         }
     }
 }
